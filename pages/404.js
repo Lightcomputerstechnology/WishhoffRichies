9a4189fadc58135c56ffe128291d06a106d0f4da
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-10-17 00:29:12 +0100

    Add custom 404 page component

---
+++ pages/404.js
@@ -0,0 +1,8 @@
+export default function Custom404() {
+  return (
+    <main>
+      <h1>404 - Page Not Found</h1>
+      <p>Oops! The page you’re looking for doesn’t exist.</p>
+    </main>
+  );
+}
