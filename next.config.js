bb480c1b8391e6cf851e44781ebad717f43d9f1c
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-10-17 19:52:12 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: { domains: ["images.unsplash.com", "your-supabase-bucket.supabase.co"] }
 };
-
 module.exports = nextConfig;
