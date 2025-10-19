5c11227503542edb158b2c660669c5910bd7eca8
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-10-19 05:56:35 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
-// pages/index.js
 import Head from "next/head";
 import Navbar from "../components/Navbar";
 import HeroSection from "../components/HeroSection";
-import FeaturesSection from "../components/FeaturesSection";
 import FeaturedWishesSection from "../components/FeaturedWishesSection";
-import HowItWorksSection from "../components/HowItWorksSection";
 import MilestonesSection from "../components/MilestonesSection";
+import TestimonialsSection from "../components/TestimonialsSection";
+import PartnersSection from "../components/PartnersSection";
+import NewsletterSection from "../components/NewsletterSection";
+import FeaturedWishesStats from "../components/FeaturedWishesStats";
 import Footer from "../components/Footer";
+import CTASection from "../components/CTASection";
 
 export default function Home() {
   return (
@@ -21,10 +23,27 @@ export default function Home() {
 
       <Navbar />
       <HeroSection />
-      <FeaturesSection />
       <FeaturedWishesSection />
-      <HowItWorksSection />
+      <FeaturedWishesStats />
       <MilestonesSection />
+      <TestimonialsSection />
+      <PartnersSection />
+      <NewsletterSection />
+
+      <CTASection
+        title="Become a Generous Donor"
+        subtitle="Your contribution can change a life today."
+        buttonText="Start Giving"
+        buttonLink="/wish/new"
+      />
+
+      <CTASection
+        title="Join Our Mission"
+        subtitle="Sign up and start helping people now."
+        buttonText="Sign Up"
+        buttonLink="/signup"
+      />
+
       <Footer />
     </>
   );
