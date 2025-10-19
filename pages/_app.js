3054afa51c29ba6cc2852b2f1eef880dae9ab3d5
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-10-19 06:20:38 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,17 @@
 // pages/_app.js
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,     // Animation duration
+      once: true,         // Only animate once
+      easing: 'ease-out-cubic',
+    });
+  }, []);
+
+  return <Component {...pageProps} />;
 }
